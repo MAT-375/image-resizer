9a0d277bef30508d1252380b68dc9d5b05ffc1f0
MAT-375 <ahsantariq1996@gmail.com>
2023-08-22 05:50:10 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import time
+
+path = "C:\\Users\\Ahsan\\Downloads\\GMC\\"
+destination = "H:\\Python\\Projects\\image-resizer\\new_image.png"
+src = cv2.imread(path + "0.jpg", cv2.IMREAD_UNCHANGED)
+cv2.imshow("title", src)
+
+scale_percent = 50
+
+new_width = int(src.shape[1] * scale_percent / 100)
+new_height = int(src.shape[0] * scale_percent / 100)
+
+dsize = (new_width, new_height)
+
+output = cv2.resize(src, dsize)
+
+cv2.imwrite(destination, output)
+cv2.waitKey(0)
